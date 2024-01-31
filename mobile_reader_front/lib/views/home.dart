9d9630194ad6a6ed67538a50738a50575b601cbb
kevin <kevin-hamza.djeradi@epitech.eu>
2024-01-31 21:00:33 +0100

    changing home

--- mobile_reader_front/lib/views/home.dart
+++ mobile_reader_front/lib/views/home.dart
@@ -6,59 +6,67 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      padding: const EdgeInsets.all(8.0),
-      children: <Widget>[
-        const Padding(
-          padding: EdgeInsets.symmetric(vertical: 8.0),
-          child: Text(
-            'En cours',
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-        ),
-        const Center(
-          child: SizedBox(
-            height: 300,
-            child: BookTile(
-              width: 400,
-              coverUrl: 'https://via.placeholder.com/100x150',
-              title: 'When A Mage Revolts',
-              author: 'Yin Si',
-              progress: 0.8,
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        title: const Text('Accueil'),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16),
+        children: <Widget>[
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 8.0),
+            child: Text(
+              'Dernière lecture',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
           ),
-        ),
-        const Padding(
-          padding: EdgeInsets.symmetric(vertical: 8.0),
-          child: Text(
-            'Historique',
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-        ),
-        SizedBox(
-          height: 250,
-          child: ListView(
-            scrollDirection: Axis.horizontal,
-            children: const <Widget>[
-              BookTile(
-                width: 200,
+          const Center(
+            child: SizedBox(
+              height: 300,
+              child: BookTile(
+                width: 400,
                 coverUrl: 'https://via.placeholder.com/100x150',
                 title: 'When A Mage Revolts',
                 author: 'Yin Si',
                 progress: 0.8,
               ),
-              SizedBox(width: 8),
-              BookTile(
-                width: 200,
-                coverUrl: 'https://via.placeholder.com/100x150',
-                title: 'True Martial World',
-                author: 'Cocooned Cow',
-                progress: 0.98,
-              ),
-            ],
+            ),
           ),
-        ),
-      ],
+          const SizedBox(height: 20),
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 8.0),
+            child: Text(
+              'Votre historique',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          SizedBox(
+            height: 250,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: const <Widget>[
+                BookTile(
+                  width: 200,
+                  coverUrl: 'https://via.placeholder.com/100x150',
+                  title: 'When A Mage Revolts',
+                  author: 'Yin Si',
+                  progress: 0.8,
+                ),
+                SizedBox(width: 8),
+                BookTile(
+                  width: 200,
+                  coverUrl: 'https://via.placeholder.com/100x150',
+                  title: 'True Martial World',
+                  author: 'Cocooned Cow',
+                  progress: 0.98,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
