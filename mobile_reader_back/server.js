8b185b45f37d1348737260e33a731ab213bdbb42
kevin <kevin-hamza.djeradi@epitech.eu>
2024-02-10 23:59:26 +0100

    setting size limit to 50 mb

--- mobile_reader_back/server.js
+++ mobile_reader_back/server.js
@@ -21,7 +21,8 @@ mongoose.connect(dbURI)
     .catch(err => console.error('Could not connect to MongoDB', err));
 
 app.use(cors());
-app.use(express.json());
+
+app.use(express.json({ limit: '50mb' }));
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
