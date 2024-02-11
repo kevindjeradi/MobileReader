798aa0d7db53960942e10d34130da34dace09d2f
kevin <kevin-hamza.djeradi@epitech.eu>
2024-02-11 23:41:20 +0100

    addded new user data to addNovel, fixed addChapterRead route to check if chapter is already present to update it to prevent chapter duplication and adding route to adorUpdate novel to history

--- mobile_reader_back/routes/user_novel_routes.js
+++ mobile_reader_back/routes/user_novel_routes.js
@@ -8,7 +8,9 @@ const router = express.Router();
 // Route to add a novel to the user's list
 router.post('/user/addNovel', checkAuth, async (req, res) => {
     const userId = req.userId; // Assuming userId is extracted from the token by checkAuth middleware
-    const { novelTitle, description, numberOfChapters } = req.body;
+    const { novelDetails } = req.body;
+
+    const { novelTitle, description, coverUrl, author, numberOfChapters, chaptersDetails } = novelDetails;
 
     try {
         const user = await User.findById(userId);
@@ -20,17 +22,20 @@ router.post('/user/addNovel', checkAuth, async (req, res) => {
         if (!novelExists) {
             const novel = {
                 novelTitle,
+                author,
+                coverUrl,
                 description,
                 numberOfChapters,
+                chaptersDetails,
                 lastReadChapter: 0,
                 lastReadAt: Date.now(),
                 chaptersRead: []
             };
             user.novels.push(novel);
             await user.save();
-            res.status(200).json({ message: 'Novel added successfully', novel });
+            res.status(200).json({ message: 'Novel added successfully', novel, novelAdded: true });
         } else {
-            res.status(200).json({ message: 'Novel already exists', novelTitle });
+            res.status(200).json({ message: 'Novel already exists', novelTitle, novelAdded: false });
         }
     } catch (error) {
         console.error('Error adding novel to user:', error);
@@ -79,7 +84,7 @@ router.patch('/user/updateLastRead', checkAuth, async (req, res) => {
         const novelIndex = user.novels.findIndex(novel => novel.novelTitle === novelTitle);
         if (novelIndex !== -1) {
             user.novels[novelIndex].lastReadChapter = lastReadChapter;
-            user.novels[novelIndex].lastReadAt = Date.now(); // Update lastReadAt to current time
+            user.novels[novelIndex].lastReadAt = Date.now();
             await user.save();
             res.status(200).json({ message: 'Novel last read updated successfully', novelTitle, lastReadChapter });
         } else {
@@ -91,10 +96,10 @@ router.patch('/user/updateLastRead', checkAuth, async (req, res) => {
     }
 });
 
-// Route to add a chapter to the chaptersRead list of a novel
+// Route to add a chapter to the chaptersRead list or update an existing one
 router.post('/user/addChapterRead', checkAuth, async (req, res) => {
     const userId = req.userId; // Assuming userId is extracted from the token by checkAuth middleware
-    const { novelTitle, chapter, progress } = req.body; // Extracting details from request body
+    const { novelTitle, chapter } = req.body; // Extracting details from request body
 
     try {
         const user = await User.findById(userId);
@@ -103,23 +108,30 @@ router.post('/user/addChapterRead', checkAuth, async (req, res) => {
         }
 
         // Find the novel by title
-        const novel = user.novels.find(n => n.novelTitle === novelTitle);
-        if (novel) {
-            // Add the new chapter to chaptersRead, default readAt to now if not provided
-            const newChapter = {
-                chapter,
-                progress,
-                readAt: Date.now() // Default to current time
-            };
-            novel.chaptersRead.push(newChapter);
-            await user.save();
-            res.status(200).json({ message: 'Chapter added to chaptersRead successfully', novelTitle, newChapter });
-        } else {
-            res.status(404).json({ message: 'Novel not found in user list', novelTitle });
+        const novelIndex = user.novels.findIndex(n => n.novelTitle === novelTitle);
+        if (novelIndex === -1) {
+            return res.status(404).json({ message: 'Novel not found in user list', novelTitle });
         }
+
+        const novel = user.novels[novelIndex];
+        const chapterIndex = novel.chaptersRead.findIndex(c => c.chapter === chapter);
+
+        if (chapterIndex !== -1) {
+            // Chapter exists, update readAt
+            novel.chaptersRead[chapterIndex].readAt = Date.now();
+        } else {
+            // Chapter does not exist, add new
+            novel.chaptersRead.push({
+                chapter,
+                readAt: Date.now() // Default to current time
+            });
+        }
+
+        await user.save();
+        res.status(200).json({ message: 'Chapter read updated successfully', novelTitle, chapter });
     } catch (error) {
-        console.error('Error adding chapter to chaptersRead:', error);
-        res.status(500).json({ message: 'Error adding chapter to chaptersRead', error: error.message });
+        console.error('Error adding/updating chapter to/in chaptersRead:', error);
+        res.status(500).json({ message: 'Error adding/updating chapter to/in chaptersRead', error: error.message });
     }
 });
 
@@ -153,6 +165,54 @@ router.delete('/user/removeChapterRead', checkAuth, async (req, res) => {
     } catch (error) {
         console.error('Error removing chapter from chaptersRead:', error);
         res.status(500).json({ message: 'Error removing chapter from chaptersRead', error: error.message });
+    }
+});
+
+// Route to add or update a novel in the user's history, ensuring the most recently read novel is first
+router.post('/user/addOrUpdateHistory', checkAuth, async (req, res) => {
+    const userId = req.userId; // Assuming userId is extracted from the token by checkAuth middleware
+    const { novelTitle } = req.body; // Now we are only expecting novelTitle in the request body
+
+    try {
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        // Attempt to find the novel in the user's existing novels list by title
+        const novel = user.novels.find(n => n.novelTitle === novelTitle);
+        if (novel) {
+            // If the novel exists in the user's list, prepare the history entry
+            const historyEntry = {
+                novelTitle: novel.novelTitle,
+                author: novel.author,
+                coverUrl: novel.coverUrl,
+                description: novel.description,
+                numberOfChapters: novel.numberOfChapters,
+                lastReadChapter: novel.lastReadChapter,
+                lastReadAt: Date.now(), // Update lastReadAt to current time
+            };
+
+            // Check if the novel already exists in the history
+            const existingIndex = user.history.findIndex(n => n.novelTitle === novelTitle);
+            if (existingIndex !== -1) {
+                // Remove the existing entry
+                user.history.splice(existingIndex, 1);
+            }
+
+            // Add the updated entry to the beginning of the history
+            user.history.unshift(historyEntry);
+        } else {
+            // Novel not found in the user's novels list
+            return res.status(404).json({ message: 'Novel not found in user list' });
+        }
+
+        await user.save();
+        console.log('User novel history updated');
+        res.status(200).json({ message: 'History updated successfully' });
+    } catch (error) {
+        console.error('Error updating user history:', error);
+        res.status(500).json({ message: 'Error updating user history', error: error.message });
     }
 });
 
