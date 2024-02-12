e4e04338368e22b3f434f2977f445691ad51c214
kevin <kevin-hamza.djeradi@epitech.eu>
2024-02-12 22:06:00 +0100

    added user provider

---
+++ mobile_reader_front/lib/provider/user_provider.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:mobile_reader_front/helpers/logger.dart';
+import 'package:mobile_reader_front/models/novel.dart';
+import 'package:mobile_reader_front/models/user.dart'; // Import User model
+
+final String baseUrl = dotenv.env['API_URL'] ?? 'http://10.0.2.2:3000';
+
+class UserProvider extends ChangeNotifier {
+  User? _user;
+
+  // Expose user data through getters
+  String get username => _user?.username ?? '';
+  String get uniqueIdentifier => _user?.uniqueIdentifier ?? '';
+  DateTime get dateJoined => _user?.dateJoined ?? DateTime.now();
+  String get profileImage => _user?.profileImage ?? '';
+  String get theme => _user?.theme ?? '';
+  List<Map<String, dynamic>> get friends => _user?.friends ?? [];
+  List<Novel> get novels => _user?.novels ?? [];
+  List<Novel> get historyNovels => _user?.historyNovels ?? [];
+
+  // Update profile image
+  void updateProfileImage(String newImageUrl) {
+    if (_user != null) {
+      _user = User(
+        username: _user!.username,
+        uniqueIdentifier: _user!.uniqueIdentifier,
+        dateJoined: _user!.dateJoined,
+        profileImage: newImageUrl, // Update the profile image
+        theme: _user!.theme,
+        friends: _user!.friends,
+        novels: _user!.novels,
+        historyNovels: _user!.historyNovels,
+      );
+      notifyListeners();
+    }
+  }
+
+  // Update a novel's favorite status
+  void toggleFavoriteStatus(String novelTitle) {
+    if (_user != null && _user!.novels.isNotEmpty) {
+      final novelIndex =
+          _user!.novels.indexWhere((novel) => novel.novelTitle == novelTitle);
+      if (novelIndex != -1) {
+        final updatedNovel = _user!.novels[novelIndex].copyWith(
+          isFavorite: !_user!.novels[novelIndex].isFavorite,
+        );
+        // Update the novel in the list
+        _user!.novels[novelIndex] = updatedNovel;
+        Log.logger.i("Novel updated: ${updatedNovel.isFavorite}");
+        // Notify listeners about the change
+        notifyListeners();
+      }
+    }
+  }
+
+  // Set user data from a Map<String, dynamic>
+  void setUserData(Map<String, dynamic> userDetails) {
+    _user = User.fromJson(userDetails);
+    Log.logger.i('Set user data called');
+  }
+}
