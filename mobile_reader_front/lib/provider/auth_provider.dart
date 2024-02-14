51755b8b9a7e9f8d2ed2742f795a4f7bcce6d290
kevin <kevin-hamza.djeradi@epitech.eu>
2024-02-14 20:39:34 +0100

    adding auth provider

---
+++ mobile_reader_front/lib/provider/auth_provider.dart
@@ -0,0 +1,46 @@
+// auth_notifier.dart
+import 'package:flutter/foundation.dart';
+import 'package:mobile_reader_front/services/token_service.dart';
+import 'package:mobile_reader_front/services/user_service.dart';
+
+class AuthProvider extends ChangeNotifier {
+  bool _isAuthenticated = false;
+  final _tokenService = TokenService();
+  final _userService = UserService();
+
+  bool get isAuthenticated => _isAuthenticated;
+
+  Future<void> checkAuthStatus() async {
+    final token = await _tokenService.getToken();
+    if (token != null && await _userService.validateToken(token)) {
+      _isAuthenticated = true;
+    } else {
+      _isAuthenticated = false;
+    }
+    notifyListeners();
+  }
+
+  Future<bool> login(String username, String password) async {
+    try {
+      // Clear any existing token before trying to log in a new user
+      await _tokenService.deleteToken();
+
+      final response = await _userService.login(username, password);
+      if (response.containsKey('token')) {
+        await _tokenService.saveToken(response['token']);
+        _isAuthenticated = true;
+        notifyListeners();
+        return true;
+      }
+      return false;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  Future<void> logout() async {
+    await _tokenService.deleteToken();
+    _isAuthenticated = false;
+    notifyListeners();
+  }
+}
