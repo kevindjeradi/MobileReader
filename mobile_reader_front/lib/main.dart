3e5c95b40c1c1ed8eee176e1899d1952e4343833
kevin <kevin-hamza.djeradi@epitech.eu>
2024-02-20 13:56:44 +0100

    updated home and main + packages

--- mobile_reader_front/lib/main.dart
+++ mobile_reader_front/lib/main.dart
@@ -1,26 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:mobile_reader_front/helpers/auth_check.dart';
+import 'package:mobile_reader_front/provider/auth_provider.dart';
+import 'package:mobile_reader_front/provider/theme_color_scheme_provider.dart';
+import 'package:mobile_reader_front/provider/user_provider.dart';
+import 'package:mobile_reader_front/theme/theme_light.dart';
+import 'package:mobile_reader_front/views/auth/login_page.dart';
+import 'package:mobile_reader_front/views/auth/register_page.dart';
 import 'package:mobile_reader_front/views/navigation_screen.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider(create: (context) => ThemeColorSchemeProvider()),
+    ChangeNotifierProvider(create: (context) => AuthProvider()),
+    ChangeNotifierProvider(create: (context) => UserProvider()),
+  ], child: const MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeColorSchemeProvider>(context);
+
     return MaterialApp(
       title: 'K Reader',
-      theme: ThemeData.dark().copyWith(
-        primaryColor: const Color.fromARGB(255, 24, 24, 24),
-        scaffoldBackgroundColor: const Color.fromARGB(255, 24, 24, 24),
-        bottomNavigationBarTheme: BottomNavigationBarThemeData(
-          selectedItemColor: Colors.amber[800],
-          unselectedItemColor: Colors.grey,
-        ),
-      ),
-      home: const NavigationScreen(),
+      theme: themeLight(themeProvider.colorScheme, themeProvider.textTheme),
+      themeMode: ThemeMode.light,
+      home: const AuthCheck(),
+      routes: {
+        '/login': (context) => const LoginPage(),
+        '/register': (context) => const RegisterPage(),
+        '/home': (context) => const NavigationScreen(),
+      },
     );
   }
 }
--- mobile_reader_front/lib/views/home.dart
+++ mobile_reader_front/lib/views/home.dart
@@ -1,55 +1,221 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:mobile_reader_front/components/book_tile.dart';
-import 'package:mobile_reader_front/models/book.dart';
+import 'package:mobile_reader_front/handlers/novel_detail_handler.dart';
+import 'package:mobile_reader_front/models/novel.dart';
+import 'package:mobile_reader_front/provider/user_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final ScrollController _scrollController = ScrollController();
+  bool _isCollapsed = false;
+  late SharedPreferences prefs;
+  late NovelDetailHandler novelDetailHandler;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_scrollListener);
+  }
+
+  Future<void> _initPrefsAndNovelDetailHandler(Novel lastReadNovel) async {
+    prefs = await SharedPreferences.getInstance();
+
+    if (mounted) {
+      novelDetailHandler = NovelDetailHandler(
+        novel: lastReadNovel,
+        prefs: prefs,
+        context: context,
+      );
+    }
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_scrollListener);
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _scrollListener() {
+    final bool isCollapsed = _scrollController.hasClients &&
+        _scrollController.offset >
+            (MediaQuery.of(context).size.height * 0.3 - kToolbarHeight);
+
+    if (isCollapsed != _isCollapsed) {
+      setState(() {
+        _isCollapsed = isCollapsed;
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-        title: const Text('Accueil'),
-      ),
-      body: ListView(
-        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16),
-        children: <Widget>[
-          const Padding(
-            padding: EdgeInsets.symmetric(vertical: 8.0),
-            child: Text(
-              'Dernière lecture',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-          ),
-          Center(
-            child: SizedBox(
-              height: 300,
-              child: BookTile(width: 400, book: mockDataBooks[0]),
-            ),
-          ),
-          const SizedBox(height: 20),
-          const Padding(
-            padding: EdgeInsets.symmetric(vertical: 8.0),
-            child: Text(
-              'Votre historique',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-          ),
-          SizedBox(
-            height: 250,
+    final userProvider = Provider.of<UserProvider>(context);
+    final novels = userProvider.novels;
+    final historyNovels = userProvider.historyNovels;
+    String? lastReadNovelCoverUrl;
+
+    Novel? findNovelInLibrary(String novelTitle) {
+      return novels.firstWhere((novel) => novel.novelTitle == novelTitle);
+    }
+
+    if (historyNovels.isNotEmpty) {
+      final lastReadNovel =
+          findNovelInLibrary(historyNovels.first.novelTitle) ??
+              historyNovels.first;
+      lastReadNovelCoverUrl = lastReadNovel.coverUrl;
+      _initPrefsAndNovelDetailHandler(lastReadNovel);
+    }
+
+    Widget historySection = historyNovels.isNotEmpty
+        ? SizedBox(
+            height: 350,
             child: ListView.builder(
               scrollDirection: Axis.horizontal,
-              itemCount: mockDataBooks.length,
+              itemCount: historyNovels.length,
               itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.only(right: 8.0),
-                  child: BookTile(
-                    book: mockDataBooks[index],
+                final correspondingNovel =
+                    findNovelInLibrary(historyNovels[index].novelTitle);
+                return BookTile(
+                    showFavorite: false,
+                    novel: correspondingNovel ?? historyNovels[index]);
+              },
+            ),
+          )
+        : const SizedBox(
+            height: 200,
+            child: Center(child: Text("Votre historique est vide")));
+
+    Widget librarySection = novels.isNotEmpty
+        ? SizedBox(
+            height: 350,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: novels.length,
+              itemBuilder: (context, index) {
+                return BookTile(novel: novels[index]);
+              },
+            ),
+          )
+        : const SizedBox(
+            height: 200,
+            child: Center(child: Text("Aucun novel dans votre bibliothèque")));
+
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: CustomScrollView(
+        controller: _scrollController,
+        slivers: <Widget>[
+          SliverAppBar(
+            backgroundColor: Theme.of(context).colorScheme.background,
+            expandedHeight: MediaQuery.of(context).size.height * 0.3,
+            floating: false,
+            pinned: true,
+            elevation: 0,
+            flexibleSpace: LayoutBuilder(
+              builder: (BuildContext context, BoxConstraints constraints) {
+                _isCollapsed = constraints.biggest.height <= kToolbarHeight;
+                return GestureDetector(
+                  onTap: () {
+                    if (historyNovels.isNotEmpty && !_isCollapsed) {
+                      novelDetailHandler.navigateToLastReadChapter(context);
+                    }
+                  },
+                  child: FlexibleSpaceBar(
+                    centerTitle: _isCollapsed ? false : true,
+                    titlePadding: _isCollapsed
+                        ? const EdgeInsets.all(0)
+                        : const EdgeInsets.only(bottom: 16.0),
+                    background: lastReadNovelCoverUrl != null
+                        ? Stack(fit: StackFit.expand, children: [
+                            ImageFiltered(
+                              imageFilter: ImageFilter.blur(
+                                sigmaX: 5,
+                                sigmaY: 5,
+                              ),
+                              child: Image.network(
+                                lastReadNovelCoverUrl,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.center,
+                              child: Image.network(
+                                lastReadNovelCoverUrl,
+                                fit: BoxFit.contain,
+                              ),
+                            ),
+                          ])
+                        : null,
+                    title: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Align(
+                        alignment: _isCollapsed
+                            ? Alignment.centerLeft
+                            : Alignment.bottomCenter,
+                        child: Text(
+                          _isCollapsed
+                              ? 'Accueil'
+                              : 'Reprendre votre dernière lecture',
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                          textAlign:
+                              _isCollapsed ? TextAlign.left : TextAlign.center,
+                          style: _isCollapsed
+                              ? Theme.of(context).textTheme.titleLarge
+                              : TextStyle(
+                                  fontSize: 16,
+                                  color: _isCollapsed
+                                      ? Colors.black
+                                      : Colors.white,
+                                  shadows: const <Shadow>[
+                                      Shadow(
+                                        offset: Offset(0.0, 2.0),
+                                        blurRadius: 5.0,
+                                        color: Colors.black,
+                                      ),
+                                    ]),
+                        ),
+                      ),
+                    ),
                   ),
                 );
               },
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                const SizedBox(height: 20),
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    'Novels dans votre bibliothèque',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                librarySection,
+                const SizedBox(height: 20),
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    'Votre historique',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                historySection,
+              ],
             ),
           ),
         ],
