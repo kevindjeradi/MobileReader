2464ff511b4de2562d84c4377ba956f7accd36b4
kevin <kevin-hamza.djeradi@epitech.eu>
2024-02-23 21:50:50 +0100

    added completed novel route to get list of complete novels

--- mobile_reader_back_ts/routes/scraper_route.ts
+++ mobile_reader_back_ts/routes/scraper_route.ts
@@ -4,6 +4,7 @@ import cheerio from 'cheerio';
 import { NovelDetails } from '../types/novelDetails.interface';
 import { Chapter } from '../types/chapter.interface';
 import { NovelInfo } from '../types/novelInfo.interface';
+import { CompletedNovelDetails } from '../types/completedNovelDetails.interface';
 
 const router = express.Router();
 
@@ -140,6 +141,59 @@ async function fetchSearchResults(url: string) {
 
         novels.push({ imageUrl, title, novelUrl });
     });
+
+    return novels;
+}
+
+router.get('/completed-novels', async (req: Request, res: Response) => {
+    const completedNovelsUrl = `${BASE_URL}/completed-novel`;
+
+    try {
+        const novels = await fetchCompletedNovels(completedNovelsUrl);
+        res.json(novels);
+    } catch (error) {
+        const message = error instanceof Error ? error.message : 'An unknown error occurred';
+        res.status(500).json({ message });
+    }
+});
+
+async function fetchCompletedNovels(baseUrl: string) {
+    let currentPageUrl = baseUrl;
+    let hasNextPage = true;
+    let novels: Array<CompletedNovelDetails> = [];
+
+    while (hasNextPage) {
+        const { data } = await axios.get(currentPageUrl);
+        const $ = cheerio.load(data);
+
+        $('.list.list-truyen.col-xs-12 .row').each((index, element) => {
+            const imageUrlSuffix = $(element).find('.col-xs-3 img.cover').attr('src') || '';
+            const title = $(element).find('.col-xs-7 h3.truyen-title a').text().trim();
+            const novelUrlSuffix = $(element).find('.col-xs-7 h3.truyen-title a').attr('href') || '';
+            const chapterCountText = $(element).find('.col-xs-2.text-info .chapter-text b').text().trim();
+            const chapterCount = parseInt(chapterCountText, 10) || 0;
+
+            if (!imageUrlSuffix || !title || !novelUrlSuffix || isNaN(chapterCount) || chapterCount < 500) {
+                return;
+            }
+
+            const imageUrl = BASE_URL + imageUrlSuffix;
+            const novelUrl = BASE_URL + novelUrlSuffix;
+
+            novels.push({ imageUrl, title, novelUrl, chapterCount });
+        });
+
+        // Find the next page link. If it doesn't exist, set hasNextPage to false.
+        const nextPageLink = $('.pagination.pagination-sm li.next a').attr('href');
+        if (nextPageLink) {
+            currentPageUrl = BASE_URL + nextPageLink;
+        } else {
+            hasNextPage = false;
+        }
+    }
+
+    // Sort novels by chapterCount in descending order
+    novels.sort((a, b) => b.chapterCount - a.chapterCount);
 
     return novels;
 }
