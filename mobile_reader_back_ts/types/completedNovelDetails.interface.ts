794de45000b897ba4c53c94f6f8e78f3ca93dcdc
kevin <kevin-hamza.djeradi@epitech.eu>
2024-02-23 21:49:36 +0100

    added completed novel details type

---
+++ mobile_reader_back_ts/types/completedNovelDetails.interface.ts
@@ -0,0 +1,8 @@
+// types/completedNovelDetails.interface.ts
+
+export interface CompletedNovelDetails {
+    imageUrl: string;
+    title: string;
+    novelUrl: string;
+    chapterCount: number;
+}
